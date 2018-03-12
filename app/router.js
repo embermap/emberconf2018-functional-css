e693bddd1fe80437fa8e550498fdd2935ab8a311
Sam Selikoff <sam.selikoff@gmail.com>
2018-03-12 00:51:50 -0700

    move things around

--- app/router.js
+++ app/router.js
@@ -8,41 +8,22 @@ const Router = EmberRouter.extend({
 
 Router.map(function() {
   this.route('1-lecture');
-  this.route('2-lecture', function() {
-    this.route('exercise', { path: '/' });
-    this.route('mockup');
-  });
-  this.route('3-exercise', function() {
-    this.route('exercise', { path: '/' });
-    this.route('mockup');
-  });
-  this.route('4-lecture', function() {
-    this.route('exercise', { path: '/' });
-    this.route('mockup');
-  });
-  this.route('5-exercise', function() {
-    this.route('exercise', { path: '/' });
-    this.route('mockup');
-  });
-  this.route('6-lecture');
-  this.route('7-lecture', function() {
-    this.route('exercise', { path: '/' });
-    this.route('mockup');
-  });
-  this.route('8-exercise', function() {
-    this.route('exercise', { path: '/' });
-    this.route('mockup');
-  });
+  this.route('2-lecture');
+  this.route('3-exercise');
+  this.route('4-exercise');
+  this.route('5-lecture');
+  this.route('6-exercise');
+  this.route('7-exercise');
+  this.route('8-exercise');
   this.route('9-lecture');
-  this.route('10-lecture', function() {
-    this.route('exercise', { path: '/' });
-    this.route('mockup');
-  });
-  this.route('11-exercise', function() {
-    this.route('exercise', { path: '/' });
-    this.route('mockup');
-  });
+  this.route('10-exercise');
+
+  this.route('11-lecture');
   this.route('12-lecture');
+  this.route('13-exercise');
+  this.route('14-lecture');
+  this.route('15-exercise');
+  this.route('16-lecture');
 });
 
 export default Router;
